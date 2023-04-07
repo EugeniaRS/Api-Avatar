af754d011809c079319cf401881a245996fc1d13
EugeniaRS <kenayanrkt@gmail.com>
2023-04-07 16:57:12 -0600

    Creacion de relaciones

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -5,6 +5,53 @@ import Personaje from "./personaje/personaje.model.js";
 
 // encapsula mis modelos
 // provedor, contenedor
+// Relacionar Clase con Personaje
+
+// Relacion Personaje con clase
+Personaje.belongsTo(Clase, {
+    // CASCADE  recorre todos los registros alos que esten asociados al id
+    // setnull solo el unico id que elimino
+    // restict 
+
+    foreignKey: 'idClase',
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE'
+        // restriccione
+        // NonNull
+        // onDelete, onUpdate
+})
+
+Clase.hasMany(Personaje, {
+    foreignKey: 'idClase'
+})
+
+
+// relacion perosnaje con personajearma
+// pertenece
+PersonajeArma.belongsTo(Personaje, {
+    foreignKey: 'idPersonaje',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
+Personaje.hasMany(PersonajeArma, {
+    foreignKey: 'idPersonaje'
+
+})
+
+
+// Relacion Arma con  personajeArma
+PersonajeArma.belongsTo(Arma, {
+    foreignKey: 'idArma',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+
+})
+Arma.hasMany(PersonajeArma, {
+    foreignKey: 'idArma'
+})
+
+
+
 
 
 export { Clase, Arma, PersonajeArma, Personaje }
